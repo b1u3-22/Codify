3f63e2f30096d2eb54f50a9df456bf18d9f19b6c
Jiří Sedlák <jiri.s.15243@gmail.com>
2020-05-21 20:27:28 +0200

    Trying to get it working for Heroku

---
+++ src/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.scss'
+
+class Card extends React.Component{
+    render(){
+
+        if (this.props.imgloc === 'top')
+            return(
+                <div className = 'Card'>
+                    <img src = {this.props.imgsrc} alt = 'Icon for card element'></img>
+                    <p>{this.props.text}</p>
+                </div>
+            );
+    }
+}
+
+export default Card; 
