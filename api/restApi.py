6e9739cba3f5dafb7612c1853f2864094578d275
Jiří Sedlák <jiri.s.15243@gmail.com>
2020-09-05 19:56:05 +0200

    fixed port typo

--- api/restApi.py
+++ api/restApi.py
@@ -170,4 +170,4 @@ def returnAllPosts():
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", 
-            port = os.environ.get('PORT', 5000))
+            port = os.environ.get('PORT', 40))
